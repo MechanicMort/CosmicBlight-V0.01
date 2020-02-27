9545764405f2fcc24ec819dfe752c1fc53ab777d
MechanicMort <47052071+MechanicMort@users.noreply.github.com>
2020-02-27 11:29:26 +0000

    Initial commit

---
+++ Assets/Scripts/Player/Movement/PlayerLook.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensitivity = 1f;
+    public Transform playerBody;
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        MouseMovement();
+    }
+
+    private void MouseMovement()
+    {
+        float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.fixedDeltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+}
---
+++ Assets/Scripts/Player/Movement/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float raycastDistance;
+
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+
+    }
+
+    private void Update()
+    {
+        Jump();
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        // added Time.deltaTime helps with frame rate and movement issues(Cameron)
+        Vector3 movement = new Vector3(x, 0f, z) * moveSpeed * Time.deltaTime;
+        Vector3 newPosition = rb.position + transform.TransformDirection(movement);
+        rb.MovePosition(newPosition);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded())
+            {
+                rb.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, raycastDistance);
+    }
+
+
+}
