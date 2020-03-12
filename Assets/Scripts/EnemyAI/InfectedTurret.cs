081c83d7530128349880370fd0198a269a02b56a
ConnorChad <60694569+ConnorChad@users.noreply.github.com>
2020-03-12 12:58:45 +0000

    awdawd

--- Assets/Scripts/EnemyAI/InfectedTurret.cs
+++ Assets/Scripts/EnemyAI/InfectedTurret.cs
@@ -19,6 +19,7 @@ public class InfectedTurret : MonoBehaviour
 
     public Vector3 origTransform;
     public Quaternion origAngle;
+    private float timeCount = 0.0f;
 
     private void Start()
     {
@@ -37,9 +38,10 @@ public class InfectedTurret : MonoBehaviour
 
         for (int i = 0; i < targetsInView.Length; i++)
         {
+            float dist = Vector3.Distance(transform.position, targetsInView[i].transform.position);
             Transform target = targetsInView[i].transform;
             Vector3 dirToTarget = (target.position - transform.position).normalized;
-            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2 && inSight == false)
             {
                 float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
@@ -49,17 +51,16 @@ public class InfectedTurret : MonoBehaviour
                 }
             }
 
-            if (inSight)
+            else if (dist > viewRadius)
+            {
+                timeCount = timeCount + Time.deltaTime;
+                turretBarrel.transform.rotation = Quaternion.RotateTowards(transform.rotation, origAngle, 200000f );
+                inSight = false;
+            }
+            else if (inSight)
             {
                 turretBarrel.transform.LookAt(target.transform.position);
             }
-
-            float dist = Vector3.Distance(transform.position, targetsInView[i].transform.position);
-            if (dist > viewRadius)
-            {
-                inSight = false;
-            }
-
 
         }
 
