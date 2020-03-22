aa554efa2bedf370211c2620fb7a10169621e303
ConnorChad <60694569+ConnorChad@users.noreply.github.com>
2020-03-22 17:32:10 +0000

    Patrolpoint
    set up scene to demo patrol points and turret tracking and hacking (changing bool in editor)

--- Assets/Scripts/EnemyAI/EnemyAI.cs
+++ Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //For enemy FOV
+    [Header("Detection range")]
+    [Range(0, 360)] public float viewAngle;
+    public float viewDistance;
+    private NavMeshAgent enemyNav;
+
+    [Header("Patrol")]
+    public bool isPatrol;
+    public Transform[] points;
+    private int destPoint = 0;
+
+    //For detection
+    [Header("Other")]
+   // public LayerMask targetMask;
+   // public LayerMask wallMask;
+    //public LayerMask enemyLayer;
+    public bool playerDetected = false;
+
+    private void Start()
     {
-        
+        enemyNav = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Patrol();
+    }
+    public void Patrol()
+    {
+        if (isPatrol)
+        {
+            if (!playerDetected) 
+            {
+                enemyNav.stoppingDistance = 1f;
+                if (!enemyNav.pathPending && enemyNav.remainingDistance < 0.5f)
+                {
+                    GoToNextPoint();
+                }
+
+            }
+        }
+    }
+
+    public void GoToNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+
+        enemyNav.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
     }
 }
