df0a1ccfbada1e5eea15dd499810a2231aa98430
ConnorChad <60694569+ConnorChad@users.noreply.github.com>
2020-03-24 18:25:01 +0000

    more ai

---
+++ Assets/Scripts/Editor/EnemyFOVEditor.cs
@@ -0,0 +1,25 @@
+﻿#if (UNITY_EDITOR)
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof (EnemyAI))]
+public class EnemyFOVEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        EnemyAI fov = (EnemyAI)target;
+        Handles.color = Color.blue;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewDistance);
+        Vector3 viewAngleA = fov.dirFromAngle(-fov.viewAngle / 2, false);
+        Vector3 viewAngleB = fov.dirFromAngle(+fov.viewAngle / 2, false);
+
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewDistance);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewDistance);
+    }
+
+
+}
+
+#endif
--- Assets/Scripts/EnemyAI/EnemyAI.cs
+++ Assets/Scripts/EnemyAI/EnemyAI.cs
@@ -10,6 +10,7 @@ public class EnemyAI : MonoBehaviour
     [Range(0, 360)] public float viewAngle;
     public float viewDistance;
     private NavMeshAgent enemyNav;
+    public Transform player;
 
     [Header("Patrol")]
     public bool isPatrol;
@@ -18,34 +19,72 @@ public class EnemyAI : MonoBehaviour
 
     //For detection
     [Header("Other")]
-   // public LayerMask targetMask;
-   // public LayerMask wallMask;
+    public LayerMask targetMask;
+    public LayerMask wallMask;
     //public LayerMask enemyLayer;
     public bool playerDetected = false;
 
-    private void Start()
+    private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
         enemyNav = GetComponent<NavMeshAgent>();
     }
 
+
     private void Update()
     {
+        
         Patrol();
+        PlayerDetection();
+
+        if (playerDetected)
+        {
+            transform.LookAt(player.position);
+            enemyNav.SetDestination(player.position);
+        }
+
     }
+
+    private void PlayerDetection()
+    {
+        Collider[] playersInView = Physics.OverlapSphere(transform.position, viewDistance, targetMask);
+
+        for (int i = 0; i < playersInView.Length; i++)
+        {
+            Transform target = playersInView[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, wallMask))
+                {
+                    playerDetected = true;
+                }
+                else
+                {
+                    playerDetected = false;
+                }
+            }
+
+        }
+    }
+
     public void Patrol()
     {
         if (isPatrol)
         {
-            if (!playerDetected) 
+            if (!playerDetected)
             {
                 enemyNav.stoppingDistance = 1f;
                 if (!enemyNav.pathPending && enemyNav.remainingDistance < 0.5f)
                 {
                     GoToNextPoint();
                 }
-
             }
-        }
+          }
+            
+        
     }
 
     public void GoToNextPoint()
@@ -56,5 +95,14 @@ public class EnemyAI : MonoBehaviour
         enemyNav.destination = points[destPoint].position;
 
         destPoint = (destPoint + 1) % points.Length;
+    }
+
+    public Vector3 dirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
     }
 }
