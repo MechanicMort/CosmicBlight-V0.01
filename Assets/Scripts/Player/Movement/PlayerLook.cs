3ebaafe0ce6c20acfe98484f0d200d762383045f
MechanicMort <47052071+MechanicMort@users.noreply.github.com>
2020-03-25 10:30:37 +0000

    leaning done

--- Assets/Scripts/Player/Movement/PlayerLook.cs
+++ Assets/Scripts/Player/Movement/PlayerLook.cs
@@ -7,16 +7,68 @@ public class PlayerLook : MonoBehaviour
     public float mouseSensitivity = 1f;
     public Transform playerBody;
     private float xRotation = 0f;
+    public Transform _Pivot;
+    public float speed = 100f;
+    public float maxAngle = 20f;
+    public GameObject locationR;
+    public GameObject locationL;
+    public GameObject startLocation;
+    public GameObject targetLocation;
 
+    float curAngle = 0f;
+
+
+ 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        if (_Pivot == null && transform.parent != null) _Pivot = transform.parent;
     }
 
     private void Update()
     {
         MouseMovement();
+        Lean();
+        
+    }
+
+    private void Lean()
+    {
+        if (Input.GetKey(KeyCode.Q) && !canLeanLeft())
+        {
+            curAngle = Mathf.MoveTowardsAngle(curAngle, maxAngle, speed * Time.deltaTime);
+            targetLocation = locationL;
+        }
+        else if (Input.GetKey(KeyCode.E) && !canLeanRight())
+        {
+            curAngle = Mathf.MoveTowardsAngle(curAngle, -maxAngle, speed * Time.deltaTime);
+            targetLocation = locationR;
+        }
+        else
+        {
+            curAngle = Mathf.MoveTowardsAngle(curAngle, 0f, speed * Time.deltaTime);
+            targetLocation = startLocation;
+        }
+
+        if (!canLeanLeft() || !Input.GetKey(KeyCode.E) && !Input.GetKey(KeyCode.Q))
+        {
+            transform.localRotation = Quaternion.AngleAxis(curAngle, Vector3.forward);
+            transform.position = (Vector3.MoveTowards(transform.position, targetLocation.transform.position, 0.1f));
+        }
+
+    }
+
+
+    private bool canLeanLeft()
+    {
+        Debug.DrawRay(transform.position, Vector3.left, Color.red,0.3f);
+        return Physics.Raycast(transform.position, Vector3.left, 0.3f,8);
+    }
+    private bool canLeanRight()
+    {
+        Debug.DrawRay(transform.position, -Vector3.left, Color.red, 0.3f);
+        return Physics.Raycast(transform.position, -Vector3.left, 0.3f,8);
     }
 
     private void MouseMovement()
--- Assets/Scripts/Player/Movement/PlayerMove.cs
+++ Assets/Scripts/Player/Movement/PlayerMove.cs
@@ -6,7 +6,12 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private float jumpForce;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float leanSpeed;
+    [SerializeField] private float baseMoveSpeed;
     [SerializeField] private float raycastDistance;
+    [SerializeField] private float sprintMod;
+    [SerializeField] private float crouchMod;
+
 
     private Rigidbody rb;
 
@@ -25,14 +30,45 @@ public class PlayerMove : MonoBehaviour
     private void Update()
     {
         Jump();
+        Crouch();
+        Sprint();
     }
+
+    private void Crouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            crouchMod = 0.5f;
+            transform.localScale = new Vector3(1f,0.5f,1f);
+        }
+        else
+        {
+            crouchMod = 1f;
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    private void Sprint()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sprintMod = 1.5f;
+        }
+        else
+        {
+            sprintMod = 1f;
+        }
+    }
+
+   
 
     private void Movement()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        // added Time.deltaTime helps with frame rate and movement issues(Cameron)
+
+        moveSpeed = baseMoveSpeed * crouchMod * sprintMod;
         Vector3 movement = new Vector3(x, 0f, z) * moveSpeed * Time.deltaTime;
         Vector3 newPosition = rb.position + transform.TransformDirection(movement);
         rb.MovePosition(newPosition);
